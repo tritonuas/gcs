0d08a14fec9de7402e4d57801438ad79b4a4e1ed
Tyler Lentz <tlentz@ucsd.edu>
2020-11-12 20:03:36 -0800

    Finished unit testing with the unit test for ODLCs

--- internal/interopconn/client_test.go
+++ internal/interopconn/client_test.go
@@ -1,6 +1,11 @@
 package interopconn
 
 import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
 	"testing"
 )
 
@@ -122,26 +127,171 @@ func TestODLCs(t *testing.T) {
 
 	intErr := client.PostODLC(postODLC)
 	if intErr.post {
-		t.Error("Expected post error to be false, but it was true")
+		t.Error("Expected PostODLC post error to be false, but it was true")
 	}
 	if intErr.output {
-		t.Error("Expected output error to be false, but it was true")
+		t.Error("Expected PostODLC output error to be false, but it was true")
 	}
 	if postODLC.GetId() == 0 {
 		t.Error("Expected to have an updated odlc ID, but it was still 0")
 	}
 
 	// Test getting an odlc
+	getODLC, intErr := client.GetODLC(int(postODLC.GetId()))
+	if intErr.get {
+		t.Error("Expected GetODLC get error to be false, but it was true")
+	}
+	if intErr.output {
+		t.Error("Expected GetODLC output error to be false, but it was true")
+	}
+	if !compareODLCs(getODLC, postODLC) {
+		t.Error("Expected getODLC and postODLC to be equal, but they were not")
+	}
+
+	getODLCs, intErr := client.GetODLCs(-1)
+	if intErr.get {
+		t.Error("Expected GetODLCs get error to be false, but it was true")
+	}
+	if intErr.output {
+		t.Error("Expected GetODLCs output error to be false, but it was true")
+	}
+	// check if the posted odlc is in the list of odlcs
+	containsPostODLC := false
+	for _, tempODLC := range getODLCs {
+		if compareODLCs(&tempODLC, postODLC) {
+			containsPostODLC = true
+			break
+		}
+	}
+	if !containsPostODLC {
+		t.Error("Expected getODLCs to contain postODLC, but it did not")
+	}
+
+	getODLCsMission, intErr := client.GetODLCs(1)
+	if intErr.get {
+		t.Error("Expected GetODLCsMission get error to be false, but it was true")
+	}
+	if intErr.output {
+		t.Error("Expected GetODLCsMission output error to be false, but it was true")
+	}
+	// check if the posted odlc is also in this list of odlcs
+	containsPostODLC = false
+	for _, tempODLC := range getODLCsMission {
+		if compareODLCs(&tempODLC, postODLC) {
+			containsPostODLC = true
+			break
+		}
+	}
+	if !containsPostODLC {
+		t.Error("Expected getODLCsMission to contain postODLC, but it did not")
+	}
+
+	getODLCsBadMission, intErr := client.GetODLCs(2)
+	if intErr.get {
+		t.Error("Expected getODLCsBadMission get error to be false, but it was true")
+	}
+	if intErr.output {
+		t.Error("Expected getODLCsBadMission output error to be false, but it was true")
+	}
+	// check to make sure that the posted odlc is not also in this list of odlcs
+	containsPostODLC = false
+	for _, tempODLC := range getODLCsBadMission {
+		if compareODLCs(&tempODLC, postODLC) {
+			containsPostODLC = true
+			break
+		}
+	}
+	if containsPostODLC {
+		t.Error("Expected getODLCsBadMission not to contain postODLC, but it did")
+	}
 
 	// Test updating an odlc
+	putShape := Odlc_CIRCLE
+	postODLC.Shape = &putShape
+	intErr = client.PutODLC(int(postODLC.GetId()), postODLC)
+	if intErr.get {
+		t.Error("Expected PutODLC get error to be false, but it was true")
+	}
+	if intErr.output {
+		t.Error("Expected PutODLC output error to be false, but it was true")
+	}
 
 	// Test uploading an image
 
+	// Making a test image using go's image libary
+	width := 256
+	height := 256
+	img := image.NewNRGBA(image.Rect(0, 0, width, height))
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			img.Set(x, y, color.NRGBA{
+				R: uint8((x + y) & 255),
+				G: uint8((x + y) << 1 & 255),
+				B: uint8((x + y) << 2 & 255),
+				A: 255,
+			})
+		}
+	}
+	buff := new(bytes.Buffer)
+	png.Encode(buff, img)
+	b := []byte(fmt.Sprint(buff))
+	// Actually put the image to the server
+	intErr = client.PutODLCImage(int(postODLC.GetId()), b)
+	if intErr.put {
+		t.Error("Expected PutODLCIMage put error to be false, but it was true")
+	}
+
 	// Test getting an image
+	getImage, intErr := client.GetODLCImage(int(postODLC.GetId()))
+	if intErr.get {
+		t.Error("Expected GetODLCImage get error to be false, but it was true")
+	}
+	if !bytes.Equal(getImage, b) {
+		t.Error("Expected getImage to be the same as the uploaded image, but it was not")
+	}
 
 	// Test deleting an image
+	intErr = client.DeleteODLCImage(int(postODLC.GetId()))
+	if intErr.delete {
+		t.Error("Expected DeleteODLCImage delete error to be false, but it was true")
+	}
 
 	// Test deleting an odlc
+	intErr = client.DeleteODLC(int(postODLC.GetId()))
+	if intErr.delete {
+		t.Error("Expected DeleteODLC delete error to be false, but it was true")
+	}
+
+	newODLCsList, intErr := client.GetODLCs(-1)
+	containsDeletedODLC := false
+	for _, tempODLC := range newODLCsList {
+		if compareODLCs(&tempODLC, postODLC) {
+			containsDeletedODLC = true
+			break
+		}
+	}
+	if containsDeletedODLC {
+		t.Error("Expected newODLCsList to not contain the deleted ODLC, but it did")
+	}
+}
+
+func compareODLCs(odlc1, odlc2 *Odlc) bool {
+	// Have to manually define all of the comparisons because the
+	// protoImpl.MessageState won't let us just use == to compare 2 Odlc objects
+	// directly since the message state cant be compared
+	return odlc1.GetId() == odlc2.GetId() &&
+		odlc1.GetMission() == odlc2.GetMission() &&
+		odlc1.GetType() == odlc2.GetType() &&
+		odlc1.GetLatitude() == odlc2.GetLatitude() &&
+		odlc1.GetLongitude() == odlc2.GetLongitude() &&
+		odlc1.GetOrientation() == odlc2.GetOrientation() &&
+		odlc1.GetShape() == odlc2.GetShape() &&
+		odlc1.GetAlphanumeric() == odlc2.GetAlphanumeric() &&
+		odlc1.GetShapeColor() == odlc2.GetShapeColor() &&
+		odlc1.GetAlphanumericColor() == odlc2.GetAlphanumericColor() &&
+		odlc1.GetDescription() == odlc2.GetDescription() &&
+		odlc1.GetAutonomous() == odlc2.GetAutonomous()
+
 }
 
 // TODO: rest of unit tests for client functionality
