26c8cfdfe675da79fa5fc5a4d8dc213c6eb59c01
Garrett Gibo <garrett.gibo@gmail.com>
2020-11-23 14:19:22 -0800

    Create install scripts for go and protobuf-compiler

---
+++ scripts/install-protoc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+COMMAND=protoc
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# Exit script if protobuf compiler already installed
+if command -v  $COMMAND &> /dev/null; then
+    echo -e "${BLUE}${COMMAND} already installed${NC}"
+		protoc --version
+		exit
+fi
+
+# Install protobuf-compiler on linux or OSX
+echo -e "${RED}${COMMAND} could not be found"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		echo -e "${BLUE}Installing protobuf compiler on ${OSTYPE}${NC}"
+		sudo apt install -y protobuf-compiler
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+		echo -e "${BLUE}Installing protobuf compiler on ${OSTYPE}${NC}"
+		brew install protobuf
+else
+		echo -e "${RED}$OSTYPE not currently supported for script install ${NC}"
+		exit
+fi
+
+# Install protoc-gen-go for go compiling
+echo -e "${BLUE}Installing protoc-gen-go${NC}"
+go get google.golang.org/protobuf/cmd/protoc-gen-go
+
+echo -e "${BLUE}Setting PATH for protoc-gen-go${NC}"
+echo "export PATH='$PATH:$(go env GOPATH)/bin'" >> $HOME/.bashrc
