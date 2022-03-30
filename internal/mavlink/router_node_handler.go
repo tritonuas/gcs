a1a79fa9a2d8a09e859185e207c960ae0416af17
Anthony Tarbinian <atar137h@gmail.com>
2022-03-29 17:40:49 -0700

    moved node handler to separate file

---
+++ internal/mavlink/router_node_handler.go
@@ -0,0 +1,91 @@
+package mav
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/aler9/gomavlib"
+)
+
+// responsible for managing the other nodes that we are forwarding/receiving nodes to/from
+// only needed for the mavlink router that is included in hub
+const (
+	nodeInactiveAfter = 30 * time.Second
+)
+
+type remoteNode struct {
+	Channel     *gomavlib.Channel
+	SystemID    byte
+	ComponentID byte
+}
+
+func (i remoteNode) String() string {
+	return fmt.Sprintf("chan=%s sid=%d cid=%d", i.Channel, i.SystemID, i.ComponentID)
+}
+
+type nodeHandler struct {
+	remoteNodeMutex sync.Mutex
+	remoteNodes     map[remoteNode]time.Time
+}
+
+func newNodeHandler() (*nodeHandler, error) {
+	nh := &nodeHandler{
+		remoteNodes: make(map[remoteNode]time.Time),
+	}
+
+	return nh, nil
+}
+
+func (nh *nodeHandler) run() {
+	// delete remote nodes after a period of inactivity
+	for {
+		time.Sleep(10 * time.Second)
+
+		now := time.Now()
+
+		func() {
+			nh.remoteNodeMutex.Lock()
+			defer nh.remoteNodeMutex.Unlock()
+
+			for rnode, t := range nh.remoteNodes {
+				if now.Sub(t) >= nodeInactiveAfter {
+					Log.Infof("node disappeared: %s", rnode)
+					delete(nh.remoteNodes, rnode)
+				}
+			}
+		}()
+	}
+}
+
+func (nh *nodeHandler) onEventFrame(evt *gomavlib.EventFrame) {
+	rnode := remoteNode{
+		Channel:     evt.Channel,
+		SystemID:    evt.SystemID(),
+		ComponentID: evt.ComponentID(),
+	}
+
+	nh.remoteNodeMutex.Lock()
+	defer nh.remoteNodeMutex.Unlock()
+
+	// new remote node
+	if _, ok := nh.remoteNodes[rnode]; !ok {
+		Log.Infof("node appeared: %s", rnode)
+	}
+
+	// always update time
+	nh.remoteNodes[rnode] = time.Now()
+}
+
+func (nh *nodeHandler) onEventChannelClose(evt *gomavlib.EventChannelClose) {
+	nh.remoteNodeMutex.Lock()
+	defer nh.remoteNodeMutex.Unlock()
+
+	// delete remote nodes associated to channel
+	for rnode := range nh.remoteNodes {
+		if rnode.Channel == evt.Channel {
+			delete(nh.remoteNodes, rnode)
+			Log.Infof("node disappeared: %s", rnode)
+		}
+	}
+}
