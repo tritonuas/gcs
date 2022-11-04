9c6b4db4b749a63537627a71336dda822684ff56
Cody Rupp <crupp@ucsd.edu>
2022-11-03 17:46:18 -0700

    added mission timer / timer initializer

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,7 +5,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+<<<<<<< HEAD
 	"strconv"
+=======
+	"time"
+>>>>>>> deaa23d (added mission timer / timer initializer)
 
 	"github.com/gin-gonic/gin"
 
@@ -21,20 +25,16 @@ var Log = logrus.New()
 Stores the server state and data that the server deals with.
 */
 type Server struct {
-	UnclassifiedTargets []cv.UnclassifiedODLC
-	telemetryLatest     *mav.Telemetry
-	telemetryHistory    []mav.Telemetry
-	// Convert methods to use influx's data
-	influxClient utils.InfluxClient
+	UnclassifiedTargets		[]cv.UnclassifiedODLC
+	MissionTime				time.Time
 }
 
 func (server *Server) SetupRouter() *gin.Engine {
 	router := gin.Default()
 
 	router.POST("/obc/targets", server.postOBCTargets())
-	router.GET("/telemetry/latest", server.getTelemetry())
-	router.POST("/telemetry/latest", server.postTelemetry())
-	router.GET("/telemetry/history", server.getTelemetryHistory())
+	router.GET("/hub/time", server.getTimeElapsed())
+	router.POST("/hub/time", server.startMissionTimer())
 
 	return router
 }
@@ -65,83 +65,21 @@ func (server *Server) postOBCTargets() gin.HandlerFunc {
 	}
 }
 
-func (server *Server) getTelemetry() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		telem := server.telemetryLatest
-
-		if telem == nil {
-			// First telemetry hasn't been entered yet.
-			c.Status(http.StatusNoContent)
-			return
+func (server *Server) getTimeElapsed() gin.HandlerFunc {
+	return func (c *gin.Context) {
+		// if time hasn't been initialized yet, throw error
+		if (server.MissionTime == time.Time{}) {
+			c.String(http.StatusBadRequest, "ERROR: time hasn't been initalized yet") // not sure if there's a built-in error message to use here
 		} else {
-			c.JSON(http.StatusOK, *server.telemetryLatest)
-			return
+			c.String(http.StatusOK, time.Since(server.MissionTime).String())
 		}
+		
 	}
 }
 
-func (server *Server) postTelemetry() gin.HandlerFunc {
-	return func(c *gin.Context) {
-
-		jsonData, err := ioutil.ReadAll(c.Request.Body)
-		if err != nil {
-			c.Status(http.StatusBadRequest)
-			return
-		} else if jsonData == nil || len(jsonData) == 0 {
-			// Nothing was passed.
-			c.Status(http.StatusBadRequest)
-			return
-		} else {
-			newTelem := mav.Telemetry{}
-			json.Unmarshal(jsonData, &newTelem)
-			// restringifed, _ := json.Marshal(newTelem) NOTE: commenting out this line because restringified was unused -- anthony
-			if !mav.ValidateTelemetry(newTelem) {
-				c.Status(http.StatusBadRequest)
-				return
-			}
-			server.telemetryHistory = append(server.telemetryHistory, newTelem)
-			server.telemetryLatest = &newTelem
-			c.Status(http.StatusNoContent)
-			return
-		}
-	}
-}
-
-// Currently: Index back from end: 0 -> latest, 1 -> one back, etc.
-// Todo: pass timestamp and find telemetry nearest to that timestamp.
-// Todo: Use InfluxDB to look-up timestamps.
-func (server *Server) getTelemetryHistory() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		strData, err := ioutil.ReadAll(c.Request.Body)
-		if err != nil {
-			c.Status(400)
-			return
-		}
-
-		// Send everything
-		restringifed, _ := json.Marshal(server.generalHistory)
-		fmt.Printf("postTelemetry json: Unmarshalled: %s\n", restringifed)
-		c.String(400, string(restringifed))
-		return
-
-		index, err := strconv.Atoi(string(strData))
-		// Invalid request data
-		if err != nil {
-			c.Status(400)
-			return
-		}
-		// Valid, but no Telemetry to return
-		if index == 0 && server.telemetryLatest == nil {
-			c.Status(http.StatusNoContent)
-			return
-		}
-		// Invalid index to get
-		if index < 0 || index >= len(server.telemetryHistory) {
-			c.Status(400)
-			return
-		}
-		// Return the index'th telemetry back.
-		c.JSON(http.StatusOK, server.telemetryHistory[len(server.telemetryHistory)-index-1])
-		return
+func (server *Server) startMissionTimer() gin.HandlerFunc {
+	return func (c *gin.Context) {
+		server.MissionTime = time.Now()
+		c.String(http.StatusOK, "Mission timer successfully started")
 	}
 }
