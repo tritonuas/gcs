c490f54d8c5cbf2dcb04e33b67d8fcf86e8888e6
Tyler Lentz <tlentz@ucsd.edu>
2022-12-01 17:40:28 -0800

    Added CORS middleware for Access-Control-Allow-Origin header

--- internal/server/server.go
+++ internal/server/server.go
@@ -52,8 +52,28 @@ type Bottles struct {
 	Bottles []Bottle `json:"bottles"`
 }
 
+/*
+We aren't hosting this online, so it's okay to allow requests from all origins to make Houston2's life easier
+*/
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func (server *Server) SetupRouter() *gin.Engine {
 	router := gin.Default()
+	router.Use(CORSMiddleware())
 
 	router.GET("/connections", server.testConnections())
 
