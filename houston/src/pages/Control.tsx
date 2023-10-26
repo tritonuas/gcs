c6613159ac36323a24ae824fab400f62045c5692
Sharibo13 <vshree852@gmail.com>
2023-10-25 17:30:16 -0700

    Initial Controls Page layout
    - Added map, and containers to display flight telemetry data
    - Added css definitions for the containers

--- houston/src/pages/Control.tsx
+++ houston/src/pages/Control.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import "./Control.css"
 
 function Control() {
-    return <h1>todo</h1>
+    return (
+        <>
+            <main className="controls-page">
+                <div className="flight-telemetry">
+
+                </div>
+                <MapContainer style={{width: 600, height: 600}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[51.505, -0.09]}>
+                        <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+                <div className="flight-telemetry">
+
+                </div>
+            </main>
+        </>
+    );
 }
 
 export default Control;
