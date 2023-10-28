58850128c39802eeacabaecfe309ac38b606a0ec
Sharibo13 <vshree852@gmail.com>
2023-10-27 18:54:17 -0700

    implemented dynamic sizing
    - implemented dynamic size for flight telemetry container
    - implemented dynamic size for map

--- houston/src/pages/Control.tsx
+++ houston/src/pages/Control.tsx
@@ -9,7 +9,7 @@ function Control() {
                 <div className="flight-telemetry">
 
                 </div>
-                <MapContainer style={{width: 600, height: 600}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <MapContainer style={{width: "100%", height: "90%"}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
