50993bad5cb1b7918a595e0f2620b24123d41b26
Sharibo13 <vshree852@gmail.com>
2023-11-03 21:33:32 -0700

    optimised design elements, and completed layout

--- houston/src/pages/Control.tsx
+++ houston/src/pages/Control.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import "./Control.css"
 
@@ -6,47 +6,103 @@ type Unit = 'knots' | 'm/s' | 'feet' | 'meters' | 'V' | '°F' | '°C' | '';
 
 class Parameter {
     values: number[];
+    value: number;
     units: Unit[];
     unit: Unit;
     color: React.CSSProperties;
-    value: number;
+    threshold: number[];
+    index: number;
 
-    constructor(values: number[], units: Unit[], color: React.CSSProperties) {
+    constructor(values: number[], units: Unit[], threshold: number[], index: number) {
         this.values = values;
         this.units = units;
-        this.unit = unitRandomeizer(units);
-        this.value = this.values[units.indexOf(this.unit)];
-        this.color = color;
+        this.value = values[index];
+        this.unit = this.units[values.indexOf(this.value)];
+        this.threshold = threshold;
+        this.color = randomColor({values: this.values, value: this.value, threshold: this.threshold});
+        this.index = index;
     }
 }
 
-function unitRandomeizer(units: Unit[]): Unit {
-    const randomIndex = Math.floor(Math.random() * units.length);
-    return units[randomIndex];
+function valueRandomizer() { //for speed
+    let number = [60, 120, 240];
+    const randomIndex = Math.floor(Math.random() * number.length);
+    return ([number[1], parseFloat((number[1]*1.94384).toFixed(2))]);
+}
+
+function valueRandomizer2() { //for altitude
+    let number = [60, 120, 240];
+    const randomIndex = Math.floor(Math.random() * number.length);
+    return ([number[1], parseFloat((number[1]*0.3048).toFixed(2))]);
+}
+
+function valueRandomizer3() { //for ESC temperature
+    let number = [60, 120, 240];
+    const randomIndex = Math.floor(Math.random() * number.length);
+    return ([number[1], parseFloat(((number[1]-32) * (5/9)).toFixed(2))]);
+}
+
+function valueRandomizer4() { //for battery
+    let number = [60, 120, 240];
+    const randomIndex = Math.floor(Math.random() * number.length);
+    return ([number[1], number[1]]);
+}
+
+function randomColor({values, value, threshold}: {values: number[], value: number, threshold: number[]}) {
+    if (value >= threshold[(values.indexOf(value)) * 2] && value <= threshold[(values.indexOf(value)) * 2 + 1]) {
+        return { backgroundColor: 'var(--success-text)' };
+    }
+    else {
+        return { backgroundColor: 'var(--failure-text)' };
+    }
+};
+
+function TelemetryGenerator({ key, heading, color, value, unit, onClick }: { key: number, heading: string, color: React.CSSProperties, value: number, unit: Unit, onClick: any}) {
+    return (
+        <div key={key} style={color} className='flight-telemetry' onClick={onClick}>
+            <h1 className='heading  '>{heading}</h1>
+            <p className='data'>{value} {unit}</p>
+        </div>
+    );
 }
 
 function Control() {
-    const randomColor = () => {
-        const colors = ['var(--success-text)', 'var(--failure-text)'];
-        const randomIndex = Math.floor(Math.random() * colors.length);
-        let color = colors[randomIndex];
-        return color;
+    const [index, setIndex] = useState<number[]>([0, 0, 0, 0, 0, 0, 0]);
+
+    const handleClick = (key : number) => {
+        setIndex(prevIndices => {
+            const newIndices = [...prevIndices];
+            newIndices[key] = newIndices[key] === 0 ? 1 : 0;
+            return newIndices;
+        });
     };
 
-    const generateColor = (): React.CSSProperties => {
-        return { backgroundColor: randomColor() };
-    };
-    
-    let airspeed = new Parameter([120, parseFloat((120*1.94384).toFixed(2))], ['knots', 'm/s'], generateColor());
-    let groundspeed = new Parameter([120, parseFloat((120*1.94384).toFixed(2))], ['knots', 'm/s'], generateColor());
-    let altitudeMSL = new Parameter([120, parseFloat((120*0.3048).toFixed(2))], ['feet', 'meters'], generateColor());
-    let altitudeAGL = new Parameter([120, parseFloat((120*0.3048).toFixed(2))], ['feet', 'meters'], generateColor());
-    let motorBattery = new Parameter([120], ['V'], generateColor());
-    let pixhawkBattery = new Parameter([120], ['V'], generateColor());
-    let ESCtemperature = new Parameter([120, parseFloat(((120 - 32)*(5/9)).toFixed(2))], ['°F', '°C'], generateColor());
+    let airspeedVal = valueRandomizer();
+    let groundspeedVal = valueRandomizer();
+    let altitudeMSLVal = valueRandomizer2();
+    let altitudeAGLVal = valueRandomizer2();
+    let motorBatteryVal = valueRandomizer4();
+    let pixhawkBatteryVal = valueRandomizer4();
+    let ESCtemperatureVal = valueRandomizer3();
+
+    const airspeedThreshold = [80, 160, parseFloat((80*1.94384).toFixed(2)), parseFloat((160*1.94384).toFixed(2))];
+    const groundspeedThreshold = [80, 160, parseFloat((80*1.94384).toFixed(2)), parseFloat((160*1.94384).toFixed(2))];
+    const altitudeMSLThreshold = [80, 160, parseFloat((80*0.3048).toFixed(2)), parseFloat((160*0.3048).toFixed(2))];
+    const altitudeAGLThreshold = [80, 160, parseFloat((80*0.3048).toFixed(2)), parseFloat((160*0.3048).toFixed(2))];
+    const motorBatteryThreshold = [80, 160, 80, 160];
+    const pixhawkBatteryThreshold = [80, 160, 80, 160];
+    const ESCtemperatureThreshold = [80, 160, parseFloat(((80-32) * (5/9)).toFixed(2)), parseFloat(((160-32) * (5/9)).toFixed(2))];
+
+    let airspeed = new Parameter(airspeedVal, ['knots', 'm/s'], airspeedThreshold, index[0]);
+    let groundspeed = new Parameter(groundspeedVal, ['knots', 'm/s'], groundspeedThreshold, index[1]);
+    let altitudeMSL = new Parameter(altitudeMSLVal, ['feet', 'meters'], altitudeMSLThreshold, index[2]);
+    let altitudeAGL = new Parameter(altitudeAGLVal, ['feet', 'meters'], altitudeAGLThreshold, index[3]);
+    let motorBattery = new Parameter(motorBatteryVal, ['V', 'V'], motorBatteryThreshold, index[4]);
+    let pixhawkBattery = new Parameter(pixhawkBatteryVal, ['V', 'V'], pixhawkBatteryThreshold, index[5]);
+    let ESCtemperature = new Parameter(ESCtemperatureVal, ['°F', '°C'], ESCtemperatureThreshold, index[6]);
     
     let flightMode = 'idk';
-    let flightModeColor = generateColor();
+    let flightModeColor = { backgroundColor: 'var(--highlight)' };
     
     return (
         <>
@@ -55,22 +111,10 @@ function Control() {
                     <div className='flight-telemetry' id='compass'>
                         <h1>/*insert compass*/</h1>
                     </div>
-                    <div style={airspeed.color} className='flight-telemetry' id='airspeed'>
-                        <h1>Airspeed</h1>
-                        <p className='data'>{airspeed.value} {airspeed.unit}</p>
-                    </div>
-                    <div style={groundspeed.color} className='flight-telemetry' id='groundspeed'>
-                        <h1>Groundspeed</h1>
-                        <p className='data'>{groundspeed.value} {groundspeed.unit}</p>
-                    </div>
-                    <div style={altitudeMSL.color} className='flight-telemetry' id='altitudeMSL'>
-                        <h1>Altitude MSL</h1>
-                        <p className='data'>{altitudeMSL.value} {altitudeMSL.unit}</p>
-                    </div>
-                    <div style={altitudeAGL.color} className='flight-telemetry' id='altitudeAGL'>
-                        <h1>Altitude AGL</h1>
-                        <p className='data'>{altitudeAGL.value} {altitudeAGL.unit}</p>
-                    </div>
+                    <TelemetryGenerator key={0} heading='Airspeed' color={airspeed.color} value={airspeed.value} unit={airspeed.unit} onClick={() => handleClick(0)}/>
+                    <TelemetryGenerator key={1} heading='Groundspeed' color={groundspeed.color} value={groundspeed.value} unit={groundspeed.unit} onClick={() => handleClick(1)}/>
+                    <TelemetryGenerator key={2} heading='Altitude MSL' color={altitudeMSL.color} value={altitudeMSL.value} unit={altitudeMSL.unit} onClick={() => handleClick(2)}/>
+                    <TelemetryGenerator key={3} heading='Altitude AGL' color={altitudeAGL.color} value={altitudeAGL.value} unit={altitudeAGL.unit} onClick={() => handleClick(3)}/>
                 </div>
                 <MapContainer className={"map"} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
                     <TileLayer
@@ -92,18 +136,9 @@ function Control() {
                         <h1>Flight Mode</h1>
                         <p className='data'>{flightMode}</p>
                     </div>
-                    <div style={motorBattery.color} className='flight-telemetry' id='motor-battery'>
-                        <h1>Motor Battery</h1>
-                        <p className='data'>{motorBattery.value} {motorBattery.unit}</p>
-                    </div>
-                    <div style={pixhawkBattery.color} className='flight-telemetry' id='pixhawk-battery'>
-                        <h1>Pixhawk Battery</h1>
-                        <p className='data'>{pixhawkBattery.value} {pixhawkBattery.unit}</p>
-                    </div>
-                    <div style={ESCtemperature.color} className='flight-telemetry' id='ESC-temperature'>
-                        <h1>ESC Temperature</h1>
-                        <p className='data'>{ESCtemperature.value} {ESCtemperature.unit}</p>
-                    </div>
+                    <TelemetryGenerator key={4} heading='Motor Battery' color={motorBattery.color} value={motorBattery.value} unit={motorBattery.unit} onClick={() => handleClick(4)}/>
+                    <TelemetryGenerator key={5} heading='Pixhawk Battery' color={pixhawkBattery.color} value={pixhawkBattery.value} unit={pixhawkBattery.unit} onClick={() => handleClick(5)}/>
+                    <TelemetryGenerator key={6} heading='ESC Temperature' color={ESCtemperature.color} value={ESCtemperature.value} unit={ESCtemperature.unit} onClick={() => handleClick(6)}/>
                 </div>
             </main>
         </>
