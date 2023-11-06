99ac3465a5dac2c642883f816730f943b34fe1c5
Tyler Lentz <tlentz@ucsd.edu>
2023-11-05 16:09:08 -0800

    add starting layout for input page

--- houston/src/pages/Input.tsx
+++ houston/src/pages/Input.tsx
@@ -1,4 +1,67 @@
+import { useState } from 'react';
+
 import './Input.css';
+import TuasMap from '../components/TuasMap';
+
+enum MapMode {
+    FlightBound,
+    SearchBound,
+    Waypoint
+}
+
+/**
+ * Form that contains all of the controls for entering flight boundary, search boundary,
+ * and waypoint data for the mission
+ * @returns Map Input Form
+ */
+function MapInputForm() {
+    const [mapMode, setMapMode] = useState<MapMode>(MapMode.FlightBound);
+
+    return (
+        <>
+            <form className="tuas-form">
+                <fieldset>
+                    <legend>Map Input</legend>
+                    <div>
+                        {
+                            Object.keys(MapMode).filter((v => isNaN(Number(v)))).map((v, i) => {
+                                return (
+                                    <input 
+                                        key={i} 
+                                        data-selected={mapMode == MapMode[v as keyof typeof MapMode]}
+                                        type="button" 
+                                        value={v} 
+                                        onClick={() => {
+                                            setMapMode(MapMode[v as keyof typeof MapMode]);
+                                        }}
+                                        />
+                                )
+                            })
+                        }
+                    </div>
+                </fieldset>
+            </form>
+        </>
+    );
+}
+
+/**
+ * Form that handles all the input for entering bottle loading information
+ * on the plane for the mission
+ * @returns Bottle Input Form
+ */
+function BottleInputForm() {
+    return (
+        <>
+            <form className="tuas-form">
+                <fieldset>
+                    <legend>Bottle Input</legend>
+                </fieldset>
+            </form>
+        </>
+    );
+
+}
 
 /**
  * 
@@ -7,8 +70,12 @@ import './Input.css';
 function Input() {
     return (
         <>
-            <main id="input-page">
-
+            <main className="input-page">
+                <TuasMap className="input-map" lat={51} lng={10}/>
+                <div className="right-container">
+                    <MapInputForm />
+                    <BottleInputForm />
+                </div>
             </main>
         </>
     );
