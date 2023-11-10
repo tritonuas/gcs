46a2aa5a0ab7fcdccac8312f23854f6e4b62c08b
Sharibo13 <vshree852@gmail.com>
2023-11-09 17:44:17 -0800

    build an initial framework for the report page

--- houston/src/pages/Report.tsx
+++ houston/src/pages/Report.tsx
@@ -1,7 +1,62 @@
 import React from "react"
+import TuasMap from '../components/TuasMap.tsx'
+import "./Report.css"
 
-const Report: React.FC = () => {
-    return <h1>todo</h1>
+/**
+ * @returns report page
+ */
+function Report() {
+  return (
+    <main className="report-page">
+        <div className="gallery-container">
+            <div className="unmatched-gallery">
+                <div className="image"> 
+                </div>
+                <div className="image"> 
+                </div>
+                <div className="image"> 
+                </div>
+                <div className="image"> 
+                </div>
+                <div className="image"> 
+                </div>
+                <div className="image"> 
+                </div>
+                <div className="image"> 
+                </div>
+                <div className="image"> 
+                </div>
+                <div className="image"> 
+                </div>
+                <div className="image"> 
+                </div>
+                <div className="image"> 
+                </div>
+                <div className="image"> 
+                </div>
+                <div className="image"> 
+                </div>
+                <div className="image"> 
+                </div>
+                <div className="image"> 
+                </div>
+            </div>
+            <div className="matched-gallery">
+                <div className="image"> 
+                </div>
+                <div className="image"> 
+                </div>
+                <div className="image"> 
+                </div>
+                <div className="image"> 
+                </div>
+                <div className="image"> 
+                </div>
+            </div>
+        </div>
+        <TuasMap className={'report-page-map'} lat={1.3467} lng={103.9326}/>
+    </main>
+  )
 }
 
 export default Report;
